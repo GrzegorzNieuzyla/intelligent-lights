b57912f81193bb7b7573c8f956f9cf5ab7ea19b7
Dawid Karolewski <dkarolewski@student.agh.edu.com>
2021-12-19 12:32:49 +0100

    Support walls in illuminance calculation

--- intelligent_lights/core/illuminance_calculator.py
+++ intelligent_lights/core/illuminance_calculator.py
@@ -1,7 +1,6 @@
-import numpy as np
 from math import sqrt, floor
 
-from intelligent_lights.cells.wall import Wall
+from intelligent_lights.cells.cell_type import CellType
 from intelligent_lights.visualization.visualization_context import VisualizationContext
 
 
@@ -20,8 +19,8 @@ class IlluminanceCalculator:
         cell_size = 20
         E = 0
         for x_light, y_light in context.light_positions:
-            # if IlluminanceCalculator.is_wall_in_straight_line(x, y, y_light, x_light, context):
-            #     continue
+            if IlluminanceCalculator.is_wall_in_straight_line(x, y, x_light, y_light, context):
+                continue
             dx = (x - x_light) * cell_size
             dy = (y - y_light) * cell_size
             d = sqrt(dx ** 2 + dy ** 2)
@@ -35,26 +34,32 @@ class IlluminanceCalculator:
     @staticmethod
     def is_wall_in_straight_line(x1: int, y1: int, x2: int, y2: int, context: VisualizationContext):
         line = []
-        xx = [x1, x2]
-        yy = [y1, y2]
-        coeff = np.polyfit(xx, yy, 1)
-        a = round(coeff[0], 3)
-        b = round(coeff[1], 3)
-        if abs(a) > 1:
-            for y in range(min(y1, y2), max(y1, y2)):
+
+        if x2 - x1 != 0:
+            a = round((y2 - y1) / (x2 - x1), 3)
+            b = round(-x1 * (y2 - y1) / (x2 - x1) + y1, 3)
+        else:
+            a = 1e12
+            b = 1e12
+
+        if abs(a) > 1 and a != 1e12:
+            for y in range(min(y1, y2) + 1, max(y1, y2)):
                 x = floor((y - b) / a)
                 line.append((x, y))
-        elif a == 0:
-            for x in range(min(x1, x2), max(x1, x2)):
-                line.append((x, x + 1))
-        else:
-            for x in range(min(x1, x2), max(x1, x2)):
+        if a == 0:
+            for x in range(min(x1, x2) + 1, max(x1, x2)):
+                line.append((x, y1))
+        if a == 1e12:
+            for y in range(min(y1, y2) + 1, max(y1, y2)):
+                line.append((x1, y))
+        if abs(a) <= 1:
+            for x in range(min(x1, x2) + 1, max(x1, x2)):
                 y = floor(a * x + b)
                 line.append((x, y))
+
         for p in line:
-            x = p[0]
-            y = p[1]
-            if isinstance(context.grid[x][y], Wall):
+            x, y = p
+            if context.grid[y][x].cell_type == CellType.Wall:
                 return True
 
         return False
